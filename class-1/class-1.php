736cd8f5aa4c1a5b41b73594ad07e5228b685aba
[Rayanuc] <[rayhanctg4@gmail.com]>
2019-11-13 19:02:22 +0600

    Initial commit

---
+++ class-1/class-1.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>PHP-7 Class 1.1</title>
+
+        <!-- bootstrap css link -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <!-- font-awesome css link -->
+        <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+        <!-- style css link -->
+        <link href="assets/css/style.css" rel="stylesheet">
+        <!-- responsive css link -->
+        <link href="assets/css/responsive.css" rel="stylesheet">
+        <!-- css link end -->
+        
+    </head>
+    <body>
+        
+        <div class="content-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <?php
+                        echo "<h1 class='text-center'>PHP-7</h1> \n<br/>";
+                        echo "<h3 class='text-center'>১.১ - পিএইচপি ডেভেলপমেন্ট এনভায়রনমেন্ট</h3> \n<br/>";
+                        echo "<h4>Class 1</h4> \n<br/>";
+                        echo "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum ut maxime explicabo dolore, reiciendis consectetur? Architecto deserunt vel consequatur rerum.</p>";
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+
+        <!-- jquery js link -->
+        <script src="assets/js/jquery.min.js"></script>
+        <!-- bootstrap js link -->
+        <script src="assets/js/bootstrap.min.js"></script>
+        <!-- main js link -->
+        <script src="assets/js/main.js"></script>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP 7 Tutorial</title>
+
+	<!-- bootstrap css link -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- font-awesome css link -->
+    <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+    <!-- style css link -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- responsive css link -->
+    <link href="assets/css/responsive.css" rel="stylesheet">
+    <!-- css link end -->
+
+</head>
+<body>
+    <div class="heading">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1><?php echo "All PHP class of PHP 7 Tutorial" ?></h1>
+                    <p><?php echo "Click on the class name to see the code and result." ?></p>
+                </div>
+            </div>
+        </div>
+    </div>
+	<div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="header-menu">
+                    <ul>
+                        <h1 class="chapter"><?php echo "Chapter 1: ১ - পিএইচপির প্রাথমিক বিষয়সমূহ"; ?></h1>
+                        
+                        <li><a href="class-1/class-1.php" target="_blank"><?php echo "১.১ - পিএইচপি ডেভেলপমেন্ট এনভায়রনমেন্ট" ?></a></li>
+                        <li><a href="class-2/class-2.php" target="_blank"><?php echo "১.২ - ভ্যারিয়েবল পরিচিতি" ?></a></li>
+                        <li><a href="class-3/class-3.php" target="_blank"><?php echo "১.৩ - ভ্যারিয়েবল নিয়ে আরও কথা, কনস্ট্যান্ট এবং কমেন্টস" ?></a></li>
+                        <li><a href="class-4/class-4.php" target="_blank"><?php echo "১.৪ - পিএইচপিতে আউটপুট প্রিন্ট করা নিয়ে বিস্তারিত" ?></a></li>
+                        <li><a href="class-5/class-5.php" target="_blank"><?php echo "১.৫ - উইন্ডোজের জন্য ভিজ্যুয়াল স্টুডিও তে কোডরানার ফিক্স করা (পিএইচপি নট ফাউন্ড)" ?></a></li>
+                        <li><a href="class-6/class-6.php" target="_blank"><?php echo "১.৬ - উইন্ডোজের জন্য পিএইচপিস্টর্মে তে কোডরানার ফিক্স করা" ?></a></li>
+                        <li><a href="class-7/class-7.php" target="_blank"><?php echo "১.৭ - পিএইচপিতে যোগ বিয়োগ গুন ভাগ" ?></a></li>
+                        <li><a href="class-8/class-8.php" target="_blank"><?php echo "১.৮ - ++ এবং -- নিয়ে আরও কিছু কথা" ?></a></li>
+                        <li><a href="class-9/class-9.php" target="_blank"><?php echo "১.৯ - বিভিন্ন রকমের নাম্বার সিস্টেম নিয়ে আলোচনা" ?></a></li>
+                        <li><a href="class-10/class-10.php" target="_blank"><?php echo "১.১০ - প্রিন্টএফ ফাংশনের জাদু" ?></a></li>
+                        <li><a href="class-11/class-11.php" target="_blank"><?php echo "১.১১ - sprintf নিয়ে কিছু কথা" ?></a></li>
+                        <li><a href="class-12/class-12.php" target="_blank"><?php echo "১.১২ - পিএইচপিতে কন্ডিশন (লজিক) এবং লজিকাল অপারেটর নিয়ে আলোচনা" ?></a></li>
+                        <li><a href="class-13/class-13.php" target="_blank"><?php echo "১.১৩ - ইফ এলস, লিপ ইয়ার এবং কিভাবে জটিল কন্ডিশন গুলো শর্টকাটে লেখা যায়" ?></a></li>
+                        <li><a href="class-14/class-14.php" target="_blank"><?php echo "১.১৪ - নেস্টেড ইফ এলস এবং কোডের রিডেবিলিটি" ?></a></li>
+                        <li><a href="class-15/class-15.php" target="_blank"><?php echo "১.১৫ - পিএইচপিতে টার্নারী অপারেটর এবং নেস্টেড টার্নারী অপারেটর নিয়ে কাজ করা" ?></a></li>
+                        <li><a href="class-16/class-16.php" target="_blank"><?php echo "১.১৬ - কন্ডিশন চেক করার আরেকটি উপায় - সুইচকেস" ?></a></li>
+                        <li><a href="class-17/class-17.php" target="_blank"><?php echo "১.১৭ - সুইচকেস নিয়ে আরও কথা" ?></a></li>
+                        <li><a href="class-18/class-18.php" target="_blank"><?php echo "১.১৮ - সুইচ কেসের একটা মজার ইস্যু" ?></a></li>
+                        <li><a href="class-19/class-19.php" target="_blank"><?php echo "১.১৯ - পিএইচপিতে অপারেটর প্রিসিডেন্স - কে আর আগে রান করে" ?></a></li>
+                        <li>
+                            <a href="class-20/class-20.php" target="_blank">
+                                <?php echo "১.২০ - পিএইচপিতে কন্ট্রোল স্ট্রাকচারের অল্টারনেটিভ সিনট্যাক্স নিয়ে আলোচনা" ?>
+                            </a>
+                        </li>
+                        <li><a href="class-21/class-21.php" target="_blank">
+                                <?php echo "১.২১ - টার্নারি অপারেটর নিয়ে কিছু জিনিস পরিষ্কার করা" ?> 
+                            </a>
+                        </li>
+                        <li><a href="class-22/class-22.php" target="_blank">
+                                <?php echo "১.২২ - বিভিন্ন রকম লুপের সাথে পরিচয়" ?> 
+                            </a>
+                        </li>
+                        <li><a href="class-23/class-23.php" target="_blank">
+                                <?php echo "১.২৩ - ফর লুপে মাল্টিপল স্টেপিং" ?> 
+                            </a>
+                        </li>
+                        <li><a href="class-24/class-24.php" target="_blank">
+                                <?php echo "১.২৪ - ফর লুপ দিয়ে কোন নাম্বারের ফ্যাক্টোরিয়াল বের করা" ?> 
+                            </a>
+                        </li>
+                        <li><a href="class-25/class-25.php" target="_blank">
+                                <?php echo "১.২৫ - স্মার্ট কোডিং ভার্সেস ডাম্ব কোডিং - কেন নিজের কোড নিজে রিভিউ করা প্রয়োজন" ?> 
+                            </a>
+                        </li>
+                        <li><a href="class-26/class-26.php" target="_blank">
+                                <?php echo "১.২৬ - ফর লুপে মাল্টিপল স্টেপিং নিয়ে আরও কিছু কথা" ?> 
+                            </a>
+                        </li>
+                        <li><a href="class-27/class-27.php" target="_blank">
+                                <?php echo "১.২৭ - হোয়াইল লুপে ++ নিয়ে যেটা মাথায় রাখতে হবে" ; ?> 
+                            </a>
+                        </li>
+                        <li><a href="class-28/class-28.php" target="_blank">
+                                <?php echo "১.২৮ - লুপের মাঝে কন্টিনিউ এবং ব্রেক" ; ?> 
+                            </a>
+                        </li>
+                        <li><a href="class-29/class-29.php" target="_blank">
+                                <?php echo "১.২৯ - লুপ দিয়ে ফিবোনাচ্চি সিরিজ প্রিন্ট করা" ; ?> 
+                            </a>
+                        </li>
+                        <li><a href="class-30/class-30.php" target="_blank">
+                                <?php echo "১.৩০ - স্পেসশিপ অপারেটর" ; ?> 
+                            </a>
+                        </li>
+                        <li><a href="class-31/class-31.php" target="_blank">
+                                <?php echo "১.৩১ - নাল কোলেস অপারেটর" ; ?> 
+                            </a>
+                        </li> 
+
+                        <!-- Chapter 2 link started from here -->
+                        <h1 class="chapter"><?php echo "Chapter 2: ২ - ফাংশন"; ?></h1>
+                        <li><a href="class-2.1/class-2.1.php" target="_blank">
+                                <?php echo "২.১ - ফাংশন কি এবং কিভাবে লিখতে হয়" ; ?> 
+                            </a>
+                        </li>
+                        <li><a href="class-2.2/class-2.2.php" target="_blank">
+                                <?php echo "২.২ - ফাংশন প্যারামিটারের টাইপ হিন্টিং বা টাইপ চেক" ; ?> 
+                            </a>
+                        </li>
+                        <li><a href="class-2.3/class-2.3.php" target="_blank">
+                                <?php echo "২.৩ - ফাংশন প্যারামিটারের ডিফল্ট ভ্যালু বা অপশনাল প্যারামিটার" ; ?> 
+                            </a>
+                        </li>
+                        <li><a href="class-2.4/class-2.4.php" target="_blank">
+                                <?php echo "২.৪ - ফাংশনের রিটার্ন টাইপ ফিক্স করে দেয়া" ; ?> 
+                            </a>
+                        </li>
+                        <li><a href="class-2.5/class-2.5.php" target="_blank">
+                                <?php echo "২.৫ - ফাংশনে আনলিমিটেড আর্গুমেন্ট অ্যাক্সেপ্ট করা" ; ?> 
+                            </a>
+                        </li>
+                        <li><a href="class-2.6/class-2.6.php" target="_blank">
+                                <?php echo "২.৬ - একটা বড় ফাংশনকে ছোট ফাংশনে ভাগ করা" ; ?> 
+                            </a>
+                        </li>
+                        <li><a href="class-2.7/class-2.7.php" target="_blank">
+                                <?php echo "২.৭ - রিকার্সন এবং রিকার্সিভ ফাংশন" ; ?> 
+                            </a>
+                        </li>
+                        <li><a href="class-2.8/class-2.8.php" target="_blank">
+                                <?php echo "২.৮ - রিকার্সিভ ফাংশনের সাহায্যে ফিবোনাচ্চি সিরিজ প্রিন্ট করা" ; ?> 
+                            </a>
+                        </li>
+                        <li><a href="class-2.9/index.php" target="_blank">
+                                <?php echo "২.৯ - পিএইচপিতে ভ্যারিয়েবল স্কোপ (খুবই ইম্পর্ট্যান্ট)"; ?> 
+                            </a>
+                        </li>
+                        <li><a href="class-2.10/index.php" target="_blank">
+                                <?php echo "২.১০ - রিকার্সিভ ফাংশনের সাহায্যে ফ্যাক্টোরিয়াল বের করা"; ?>
+                            </a>
+                        </li>
+
+                        <!-- Chapter 3 link started from here -->
+                        <h1 class="chapter"><?php echo "৩ - অ্যারে"; ?></h1>
+                        <li><a href="class-3.1/index.php" target="_blank">
+                                <?php echo "৩.১ - অ্যারে পরিচিতি"; ?>
+                            </a>
+                        </li>
+                        <li><a href="class-3.2/index.php" target="_blank">
+                                <?php echo "৩.২ - অ্যারে ম্যানিপুলেশন"; ?>
+                            </a>
+                        </li>
+                        <li><a href="class-3.3/index.php" target="_blank">
+                                <?php echo "৩.৩ - অ্যাসোসিয়েটিভ অ্যারে নিয়ে বিস্তারিত আলোচনা"; ?>
+                            </a>
+                        </li>
+                        <li><a href="class-3.4/index.php" target="_blank">
+                                <?php echo "৩.৪ - স্ট্রিং থেকে অ্যারে এবং অ্যারে থেকে স্ট্রিং, মাল্টিপল ডেলিমিটার"; ?>
+                            </a>
+                        </li>
+                        <li><a href="class-3.5/index.php" target="_blank">
+                                <?php echo "৩.৫ - মাল্টিডাইমেনশনাল বা নেস্টেড অ্যারে"; ?>
+                            </a>
+                        </li>
+                        <li><a href="class-3.6/index.php" target="_blank">
+                                <?php echo "৩.৬ - অ্যাসোসিয়েটিভ অ্যারে থেকে স্ট্রিংয়ে কনভার্সন - সিরিয়ালাইজ, জেসন"; ?>
+                            </a>
+                        </li>
+                        <li><a href="class-3.7/index.php" target="_blank">
+                                <?php echo "৩.৭ - কপি বাই ভ্যালু এবং কপি বাই রেফারেন্স"; ?>
+                            </a>
+                        </li>
+                        <li><a href="class-3.8/index.php" target="_blank">
+                                <?php echo "৩.৮ - অ্যাসোসিয়েটিভ অ্যারে থেকে ডেটা রিমুভ করা"; ?>
+                            </a>
+                        </li>
+
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="copyright-area-text">
+                        <p ><?php echo "© 2019 - Rayhan Uddin Chowdhury" ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+	<!-- jquery js link -->
+    <script src="assets/js/jquery.min.js"></script>
+    <!-- bootstrap js link -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- main js link -->
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
