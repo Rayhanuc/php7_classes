05197193286a14c7f020c13c483c5eb6d3887938
[Rayanuc] <[rayhanctg4@gmail.com]>
2019-11-26 23:33:30 +0600

    ৩.১৮ - অ্যারে থেকে ভ্যারিয়েবলে ডেটা নেয়ার জন্য লিস্ট ফাংশনের ব্যবহার
    ৩.১৮ - অ্যারে থেকে ভ্যারিয়েবলে ডেটা নেয়ার জন্য লিস্ট ফাংশনের ব্যবহার

---
+++ 3.18.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>PHP-7 Class 3.18</title>
+
+        <!-- bootstrap css link -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <!-- font-awesome css link -->
+        <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+        <!-- style css link -->
+        <link href="assets/css/class-style.css" rel="stylesheet">
+        <!-- responsive css link -->
+        <link href="assets/css/responsive.css" rel="stylesheet">
+        <!-- css link end -->
+        
+    </head>
+    <body>
+        
+        <div class="content-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <?php
+                        echo "<h1 class='text-center'>PHP-7</h1> \n<br/>";
+                        echo "<h3 class='text-center'>৩.১৮ - অ্যারে থেকে ভ্যারিয়েবলে ডেটা নেয়ার জন্য লিস্ট ফাংশনের ব্যবহার</h3> \n<br/>";
+                        ?>
+                        <hr>
+                        <?php echo "<h4>PHP Code: </h4>"; ?>
+                        <pre>
+
+// php code start
+
+$color = array(122,233,65);
+/*$red = $color[0];
+$green = $color[1];
+$blue = $color[2];
+
+echo $blue;*/
+
+list($red,$green,$blue) = $color;
+echo $blue;
+echo "<br/>";
+$student = array('Fname','Lname','Email','MobileNumber');
+list($firstname,$lastname,$email,$mobilenumber) = $student;
+
+echo $firstname;
+
+// php end
+
+                        </pre>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="content-area">
+
+                        <?php
+                        echo "<h4>PHP Code Result: </h4>";
+                        
+// php code start
+
+$color = array(122,233,65);
+/*$red = $color[0];
+$green = $color[1];
+$blue = $color[2];
+
+echo $blue;*/
+
+list($red,$green,$blue) = $color;
+echo $blue;
+echo "<br/>";
+$student = array('Fname','Lname','Email','MobileNumber');
+list($firstname,$lastname,$email,$mobilenumber) = $student;
+
+echo $firstname;
+
+// php end
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="copyright-area-text">
+                            <p ><?php echo "© 2019 - Rayhan Uddin Chowdhury" ?></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+
+        <!-- jquery js link -->
+        <script src="assets/js/jquery.min.js"></script>
+        <!-- bootstrap js link -->
+        <script src="assets/js/bootstrap.min.js"></script>
+        <!-- main js link -->
+        <script src="assets/js/main.js"></script>
+    </body>
+</html>
