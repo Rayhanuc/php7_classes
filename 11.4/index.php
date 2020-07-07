539dc9ab39235a27834795b129a437ba342a5dc0
[Rayanuc] <[rayhanctg4@gmail.com]>
2020-07-07 19:31:59 +0600

    11.4 - Public and private methods and properties of the class
    11.4 - Public and private methods and properties of the class

--- 11.4/index.php
+++ 11.4/index.php
@@ -35,7 +35,32 @@
 
 // php code start
 
+class Fund{
+    private $fund;
+    
+    function __construct($initialFund = 0) {
+        $this->fund = $initialFund;
+    }
 
+    public function addFund($money) {
+        $this->fund += $money;
+    }
+
+    public function deductFund($money) {
+        $this->fund -= $money;
+    }
+
+    public function getTotal() {
+        echo "Total fund is {$this->fund}\n<br/>";
+    }    
+}
+
+$ourFund = new Fund(100);
+// $ourFund->fund = 75;
+$ourFund->getTotal();
+$ourFund->addFund(10);
+$ourFund->deductFund(7);
+$ourFund->getTotal();
 
 // php end
 
