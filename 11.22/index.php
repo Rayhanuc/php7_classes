952a7deb4ebc836a448f3d1a720ea222b02a0c4a
[Rayanuc] <[rayhanctg4@gmail.com]>
2020-08-27 13:00:31 +0600

    11.22 - Compare objects
    11.22 - Compare objects

--- 11.22/index.php
+++ 11.22/index.php
@@ -34,7 +34,24 @@
 
 // php code start
 
+class Planet{
+    public $name;
 
+    function __construct($name){
+        $this->name = $name;
+    }
+}
+
+$e = new Planet("Earth");
+$e2 = $e;
+$e1 = new Planet("Earth");
+$m = new Planet("Mars");
+
+if($e1 == $e2){
+    echo "Similer Planets";
+}else {
+    echo "Not Similar";
+}
 
 // php end
 
