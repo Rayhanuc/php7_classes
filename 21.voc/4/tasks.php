2940a4d89dd1a5e380b78de7239aafbd37fa6943
[Rayanuc] <[rayhanctg4@gmail.com]>
2020-09-24 12:21:02 +0600

    21.4 - User login
    21.4 - User login

---
+++ 21.voc/4/tasks.php
@@ -0,0 +1,53 @@
+<?php
+
+include_once "config.php";
+
+$status = 0;
+$action = $_POST['action'] ?? '';
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+
+if (!$connection) {
+    throw new Exception("Cannot connect to database");
+}else {
+	if('register' == $action){
+		$username = $_POST['email'] ?? '';
+		$password = $_POST['password'] ?? '';
+		if ($username && $password) {
+			$hash = password_hash($password, PASSWORD_BCRYPT);
+			echo $hash;
+			$query = "INSERT INTO users(email, password) VALUES ('{$username}','{$hash}')";
+			mysqli_query($connection,$query);
+			if(mysqli_error($connection)){
+				$status = 1;
+			}else {
+				$status = 3;
+			}
+		}else {
+			$status = 2;
+		}
+		header("Location: index.php?status={$status}");
+	}else if('login' == $action) {
+		$username = $_POST['email'] ?? '';
+		$password = $_POST['password'] ?? '';
+		if ($username && $password) {
+			$query = "SELECT id, password FROM users WHERE email='{$username}'";
+			$result = mysqli_query($connection,$query);
+			if(mysqli_num_rows($result)>0){
+				$data = mysqli_fetch_assoc($result);
+				$_password = $data['password'];
+				if (password_verify($password, $_password)) {
+					header("Location: words.php");
+					die();
+				}else{
+					$status = 4;					
+				}
+			}else {
+				$status = 5;
+			}
+		}else{
+			$status = 2;			
+		}
+		header("Location: index.php?status={$status}");
+
+	}
+}
